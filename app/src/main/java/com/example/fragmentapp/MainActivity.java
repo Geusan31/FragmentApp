5cad09993a166c93a1fe3dd5ed4580f75850b410
Geusan31 <edurais.ariadaffa@gmail.com>
2023-10-30 12:01:07 +0700

    add fragment

--- app/src/main/java/com/example/fragmentapp/MainActivity.java
+++ app/src/main/java/com/example/fragmentapp/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.fragmentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean isFragmentDisplay=false;
+    private int mChoice=2;
+    private Button mButtonOpen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButtonOpen = findViewById(R.id.button_open);
+    }
+
+    public void openFragment(View view) {
+        if(!isFragmentDisplay) {
+            open();
+        } else {
+            close();
+        }
+    }
+
+    private void close() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        SimpleFragment simpleFragment = (SimpleFragment) fragmentManager.findFragmentById(R.id.fragment);
+        if(simpleFragment != null) {
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.remove(simpleFragment).commit();
+        }
+        isFragmentDisplay=false;
+        mButtonOpen.setText("Open");
+    }
+
+    private void open() {
+        SimpleFragment simpleFragment = SimpleFragment.newInstance(mChoice);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment, simpleFragment).addToBackStack(null).commit();
+        isFragmentDisplay=true;
+        mButtonOpen.setText("Close");
     }
 }
---
+++ app/src/main/java/com/example/fragmentapp/SimpleFragment.java
@@ -0,0 +1,50 @@
+package com.example.fragmentapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SimpleFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SimpleFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String CHOICE = "choice";
+
+    // TODO: Rename and change types of parameters
+    private int mChoice;
+
+    public SimpleFragment() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static SimpleFragment newInstance(int choice) {
+        SimpleFragment fragment = new SimpleFragment();
+        Bundle args = new Bundle();
+        args.putInt(CHOICE, choice);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_simple, container, false);
+    }
+}
